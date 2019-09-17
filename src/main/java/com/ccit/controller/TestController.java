a02b2138e62339a4d486a790a69a2a8cd8e79e2a
zhang <1455310410@qq.com>
2019-09-17 11:04:35 +0800

    自动化部署测试

---
+++ src/main/java/com/ccit/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.ccit.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+        @RequestMapping("test")
+        @ResponseBody
+        public  String toTest(){
+            return  "success";
+        }
+
+    @RequestMapping("toTry")
+    @ResponseBody
+    public  String toTry(){
+        return  "enheng";
+    }
+}
